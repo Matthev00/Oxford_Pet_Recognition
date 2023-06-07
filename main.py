13f74e04eccde4f77c4297b4806c5a03be93cb58
Matthev00 <mateuszostaszewskififa@gmail.com>
2023-06-07 11:20:46 +0200

    model trained on all 37 classes

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ import engine
 import torch # noqa 5501
 import torchvision # noqa 5501
 from torch import nn # noqa 5501
-from torchvision import transforms, datasets # noqa 5501data_20_percent_path
+from torchvision import transforms, datasets # noqa 5501
 from torchinfo import summary # noqa 5501
+from torch.utils.data import DataLoader
 
 import os
 from pathlib import Path
 import random
+from PIL import Image
 
 
 def main():
@@ -30,18 +32,9 @@ def main():
         device=device)
     train_transform = transforms.Compose(
         [transforms.TrivialAugmentWide(),
-         effnetb2_transforms])
+         resnet50_transforms])
 
-    # # # Create data (all classes)
-    # train_data, test_data = download_data(data_path=Path("data"),
-    #                                       train_transform=train_transform,
-    #                                       test_transform=effnetb2_transforms)
-    # class_names = train_data.classes
-
-    # train_data_20, _ = split_data(dataset=train_data, split=0.2)
-    # test_data_20, _ = split_data(dataset=test_data, split=0.2)
-
-    # # 3 classes
+    # # Create data 3 classes
     data_path = Path("data_3")
     train_path = data_path / "train"
     test_path = data_path / "test"
@@ -51,23 +44,6 @@ def main():
         transform=effnetb2_transforms,
         batch_size=BATCH_SIZE,
         num_workers=NUM_WORKERS)
-    # train_data = torchvision.datasets.ImageFolder(root="data_3/train",
-    #                                               transform=train_transform)
-    # test_data = torchvision.datasets.ImageFolder(root="data_3/test",
-    #                                              target_transform=effnetb2)
-    # print(train_data.classes)
-
-    # train_dataloader = DataLoader(dataset=train_data,
-    #                               batch_size=BATCH_SIZE,
-    #                               shuffle=True,
-    #                               pin_memory=True,
-    #                               num_workers=NUM_WORKERS)
-
-    # test_dataloader = DataLoader(dataset=test_data,
-    #                              batch_size=BATCH_SIZE,
-    #                              shuffle=False,
-    #                              pin_memory=True,
-    #                              num_workers=NUM_WORKERS)
 
     # # # Train model
     # optimizer = torch.optim.Adam(effnetb2.parameters(), lr=1e-2)
@@ -91,16 +67,85 @@ def main():
     model_size = Path(best_model_path).stat().st_size // (1024*1024)
     print(f"Model size {model_size}")
 
+    # # # Predict on image
+    # img_to_plot = 8
+    # test_image_path_list = list(Path(test_path).glob("*/*.jpg"))
+    # img_path_sample = random.sample(population=test_image_path_list,
+    #                                 k=img_to_plot)
+    # for img_path in img_path_sample:
+    #     utils.pred_and_plot_image(model=resnet50,
+    #                               image_path=img_path,
+    #                               class_names=class_names,
+    #                               image_size=(288, 288))
+
+    # # Train model on all classes
+
+    # Create data (all classes)
+    # Create a model
+    model, test_transforms = model_builder.create_effnetb2(num_of_classes=37,
+                                                           device=device)
+    train_transforms = transforms.Compose([transforms.TrivialAugmentWide(),
+                                           test_transforms])
+
+    train_data, test_data = download_data(data_path=Path("data"),
+                                          train_transform=train_transforms,
+                                          test_transform=test_transforms)
+    class_names = train_data.classes
+
+    train_data_20, _ = split_data(dataset=train_data, split=0.2)
+    test_data_20, _ = split_data(dataset=test_data, split=0.2)
+
+    train_dataloader = DataLoader(dataset=train_data_20,
+                                  batch_size=BATCH_SIZE,
+                                  shuffle=True,
+                                  num_workers=NUM_WORKERS,
+                                  pin_memory=True)
+
+    test_dataloader = DataLoader(dataset=test_data_20,
+                                 batch_size=BATCH_SIZE,
+                                 shuffle=False,
+                                 num_workers=NUM_WORKERS,
+                                 pin_memory=True)
+
+    # # Train model
+    # optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
+    # loss_fn = nn.CrossEntropyLoss()
+    # results = engine.train(model=model,
+    #                        train_dataloader=train_dataloader,
+    #                        test_dataloader=test_dataloader,
+    #                        optimizer=optimizer,
+    #                        loss_fn=loss_fn,
+    #                        epochs=EPOCHS,
+    #                        writer=utils.create_writer(
+    #                            experiment_name="37classes",
+    #                            model_name="effnetb2",
+    #                            extra=f'{EPOCHS}_epochs'))
+
+    # utils.plot_loss_curves(results=results)
+
+    # # Save model
+    # model_path = "Pretrianed_effnetb2_37classes.pth"
+    # utils.save_model(model=model,
+    #                  target_dir="models",
+    #                  model_name=model_path)
+
+    # # Load model
+    model_path = "models/Pretrianed_effnetb2_37classes.pth"
+    model.load_state_dict(torch.load(model_path))
+
     # # Predict on image
     img_to_plot = 8
-    test_image_path_list = list(Path(test_path).glob("*/*.jpg"))
+    test_image_path_list = list(Path("data/oxford-iiit-pet/images").glob("*.jpg"))
     img_path_sample = random.sample(population=test_image_path_list,
                                     k=img_to_plot)
     for img_path in img_path_sample:
-        utils.pred_and_plot_image(model=resnet50,
+        utils.pred_and_plot_image(model=model,
                                   image_path=img_path,
                                   class_names=class_names,
-                                  image_size=(288, 288))
+                                  transform=test_transforms)
+
+    def predict(img: Image):
+
 
 
 if __name__ == "__main__":
